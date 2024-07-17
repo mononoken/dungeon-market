3a0f8ab979cab665c8887a2eea20bb10dcbc88d2
mononoken <ken.maeshima@pm.me>
2024-07-17 10:50:34 -0700

    Add home content

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,21 @@
 export function Home() {
-  return <div>Hello, Dungeon!</div>;
+  return (
+    <div className={"container"}>
+      <h1>
+        Welcome to <em>Dungeon Market!</em>
+      </h1>
+      <p>
+        Dungeon Market is an implementation of a cart system using React, React
+        Router, and TypeScript. That's all it needs! I use localStorage to
+        persist your cart, so you can leave the page and come back to find your
+        monsters still in your cart.
+      </p>
+      <p>
+        Just another store with a cart isn't too exciting to me. After getting
+        the UI in cleaner shape, I have plans to evolve Dungeon Market into an
+        app for building scenarios for D{"&"}D adventures. In a way, you can
+        think of such an app as just a system of multiple carts!
+      </p>
+    </div>
+  );
 }
