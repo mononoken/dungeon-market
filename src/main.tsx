6582b65772486e19631fbe37c2151b45fd19f8ac
mononoken <ken.maeshima@pm.me>
2024-07-17 10:50:26 -0700

    Add gothic font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./font.css";
 import "./reset.css";
 import "./index.css";
 import { routes } from "./routes";
