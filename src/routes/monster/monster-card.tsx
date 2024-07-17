5a4d3c1dcaa5ef91d1d1364e0d929403868e4263
mononoken <ken.maeshima@pm.me>
2024-07-17 13:34:39 -0700

    Clean up card styling

--- src/routes/monster/monster-card.tsx
+++ src/routes/monster/monster-card.tsx
@@ -1,5 +1,6 @@
 import { Monster } from "../../types/monster";
 import { useCurrency } from "../../hooks/use-currency";
+import styles from "./monster-card.module.css";
 
 type MonsterCardProps = {
   monster: Monster;
@@ -10,19 +11,25 @@ export function MonsterCard({ monster, onAddToCart }: MonsterCardProps) {
   const { gold, silver } = useCurrency(monster.cr);
 
   return (
-    <div className={"card"}>
+    <div className={styles.card}>
       <h1>{monster.name}</h1>
-      <div>
-        <span>Cost: </span>
-        {gold ? <span>{`${gold}g`}</span> : null}
-        {silver ? <span>{`${silver}s`}</span> : null}
-      </div>
       <ul>
-        <li>Size: {monster.size}</li>
-        <li>Type: {monster.type}</li>
-        <li>Armor Class: {monster.armorClass}</li>
+        <li>
+          <span>Cost: </span>
+          {gold ? <span>{`${gold}g`}</span> : null}
+          {silver ? <span>{`${silver}s`}</span> : null}
+        </li>
+        <li>
+          <span>Size:</span> {monster.size}
+        </li>
+        <li>
+          <span>Type:</span> {monster.type}
+        </li>
+        <li>
+          <span>Armor Class:</span> {monster.armorClass}
+        </li>
+        <li>{monster.desc || "No description available."} </li>
       </ul>
-      <p>{monster.desc ? monster.desc : "No description available."}</p>
       <button onClick={onAddToCart}>Add to cart</button>
     </div>
   );
